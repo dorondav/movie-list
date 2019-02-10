f4d267f49daddb7f8e4ce1ff6054e32c2b9891a7
doron davidowitz <doron@evisame.com>
2019-02-10 16:56:18 +0200

    first project layout  init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './components/header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
+
+
+const routes: Routes = [
+  { path: '', component: MoviesListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+//* Angular Components */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//* Third Party Components */
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+//* Costum Components */
+import { HeaderComponent } from './components/header/header.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
+
+//* Services */
+import { MoviesDataService } from './movies/movies-data.service';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MoviesComponent,
+    MoviesListComponent,
+    MovieDetailComponent,
+    MovieEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
   ],
-  providers: [],
+  providers: [MoviesDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
